4c07ce9faec5d27ead4841602d39535cc01d8f65
Icesteel3 <118759049+icesteel3@users.noreply.github.com>
2022-11-21 13:02:49 -0500

    Add files via upload

---
+++ Drivetrain.cpp
@@ -0,0 +1,23 @@
+#include "swervedrive.hpp"
+
+class Drive {
+  SwerveModule* frontLeft;
+  SwerveModule* frontRight;
+  SwerveModule* backLeft;
+  SwerveModule* backRight;
+
+  float frontLeftAmt;
+  float frontRightAmt;
+  float backLeftAmt;
+  float backRightAmt;
+
+  frontRight -> SetInverted();
+  backRight -> SetInverted();
+
+public:
+  void getTokyoDRIFTAmt(driftX, driftY) {
+    if (driftY > 0 && driftX < 0) {
+      frontLeftAmt = 
+    }
+  }
+}
